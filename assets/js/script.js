47755d46eebb859b83ed15dbc541f9a2615995ab
vitoriacurty <vitoria.curty@hotmail.com>
2023-02-04 14:20:22 -0300

    implementação do requisito encomendar blusa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
-let name = prompt('Qual é o seu nome?')
+let nome = prompt('Qual é o seu nome?')
+
+let object = {
+  "model": null,
+  "neck": null,
+  "material":null,
+  "image": null,
+  "owner": null,
+  "author": null,
+}
+
+object.owner = nome
+object.author = nome
+object.image = input
+
+// const input = document.getElementById('input').value
+
 
 function getModelo(modelo) {
   const select = document.querySelector('.modelo .blue-border')
@@ -6,6 +22,8 @@ function getModelo(modelo) {
     select.classList.remove('blue-border')
   }
   modelo.classList.add('blue-border')
+
+  object.model = modelo.id
   activeButton()
 }
 
@@ -15,6 +33,8 @@ function getGola(gola) {
     select.classList.remove('blue-border')
   }
   gola.classList.add('blue-border')
+
+  object.neck = gola.id
   activeButton()
 }
 
@@ -25,6 +45,7 @@ function getTecido(tecido) {
   }
   tecido.classList.add('blue-border')
 
+  object.material = tecido.id
   activeButton()
 }
 
@@ -32,18 +53,55 @@ const verifyInput = document.querySelector('input')
 verifyInput.addEventListener('input', activeButton)
 
 function checkInput() {
-  input = verifyInput.value
-  if (input !== '') {
+  regexp =
+    /^(?:(?:https?):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    const input = verifyInput.value
+  object.image = input
+  if  (regexp.test(input)) {
     return true
   } else {
     return false
-  }
+  } 
 }
 
+///////////////////////////////////////////////////////
 function activeButton() {
+  
   let quantidade = document.getElementsByClassName('blue-border').length
   if (quantidade === 3 && checkInput() === true) {
     document.querySelector('button').classList.add('color-button')
     document.querySelector('button').removeAttribute('disabled')
   }
 }
+
+function confirmOrder() {
+  sendOrder()
+}
+
+/////////////////////////////////////////
+
+function sendOrder() {
+  const promise = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', object);
+  promise.then(success);
+  promise.catch(error);
+  console.log(promise)
+}
+
+////////////////////////////////////////
+
+function success(response) {
+  alert("Sua encomenda foi efetuada com sucesso!");
+  console.log(response.status);
+  console.log(response);
+  console.log(object)
+}
+
+function error(erro) {
+  alert("Ops, não conseguimos processar sua encomenda");
+  console.log(erro.response.status);
+  console.log(erro.response.data);
+}
+
+
+
+
